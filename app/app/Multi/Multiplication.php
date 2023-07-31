6a65d955115b8c0e799681520b5f78d58ec2535c
Kajojan <kojoty1999@gmail.com>
2023-07-31 21:53:27 +0200

    multiplication start and generate functions

--- app/app/Multi/Multiplication.php
+++ app/app/Multi/Multiplication.php
@@ -1,19 +1,45 @@
 <?php
 
+
 namespace App\Multi;
+
+use App\Models\Tab;
+
 
 class Multiplication
 {
 
 
-    public function generate(int $demension): array
+    public function generate(int $dimension): array
     {
+
         $res = [];
-        if (1 > $demension  || $demension > 100) {
+        for ($i = 1; $i <= $dimension; $i++) {
+            $row = [];
+            for ($j = 1; $j <= $dimension; $j++) {
+                $row[] = $i * $j;
+            }
+            $res[] = $row;
+        }
+        return $res;
+    }
+
+    public function start(int $dimension): array
+    {
+        if (1 > $dimension  || $dimension > 100) {
             throw new \InvalidArgumentException("wrong dimension");
         } else {
-
-            return $res;
+            $tab = Tab::where('number', $dimension)->first();
+            if ($tab) {
+                return json_decode($tab->array, true);
+            } else {
+                $res = $this->generate($dimension);
+                $newTab = new Tab();
+                $newTab->number = $dimension;
+                $newTab->array = json_encode($res);
+                $newTab->save();
+                return $res;
+            }
         }
     }
 }
