150aab2fa2402267284b43676db9050aedd7976a
Kajojan <kojoty1999@gmail.com>
2023-07-31 21:54:22 +0200

    adding form and display multiplication result

--- app/resources/views/test.php
+++ app/resources/views/test.php
@@ -1,21 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <p>hello</p>
+
+
+    <form method="get">
+        <input type="number" placeholder="1-100" name="size">
+        <input type="submit">
+    </form>
+
     <?php
-$conn = new mysqli("127.0.0.1", "root", "root", "my_database");
-if ($conn->connect_error) {
-    die("Nieudane połączenie: " . $conn->connect_error);
-} else {
-    echo "Połączenie z bazą danych powiodło się!";
-}
-$conn->close();
-?>
+
+    use App\Multi\Multiplication;
+
+    $tab = new Multiplication();
+    if (isset($_GET["size"])) {
+
+        $result = $tab->start($_GET["size"]);
+
+        foreach ($result as $row) {
+            echo "<p>";
+            foreach ($row as $item) {
+                echo "$item  ";
+            }
+            echo "</p>";
+        }
+        
+    }
+    ?>
 
 </body>
+
 </html>
