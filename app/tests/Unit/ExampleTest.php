dbf2892121d22cd9c4d18cf79e0f9b69f2583e3b
Kajojan <kojoty1999@gmail.com>
2023-07-31 02:27:33 +0200

    testS

--- app/tests/Unit/ExampleTest.php
+++ app/tests/Unit/ExampleTest.php
@@ -1,16 +1,55 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Multi\Multiplication;
+use App\Models\Tab;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    public function testCreateTab()
     {
-        $this->assertTrue(true);
+        $tab = new Multiplication();
+        $this->assertInstanceOf(Multiplication::class, $tab);
+    }
+
+    public function testWrongDimensionHigh()
+    {
+        $tab = new Multiplication();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $tab->generate(101);
+    }
+
+
+    public function testWrongDimensionLow()
+    {
+        $tab = new Multiplication();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $tab->generate(0);
+    }
+    public function testGenerateReturnsCorrectResultFor5x5()
+    {
+        $tab = new Multiplication();
+        $result = $tab->generate(3);
+
+        $expectedResult = [
+            [1, 2, 3],
+            [2, 4, 6],
+            [3, 6, 9],
+            [4, 8, 12],
+            [5, 10, 15],
+        ];
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testDatabase()
+    {
+        $tab = new Multiplication();
+        $result = $tab->generate(3);
+
+        $this->assertDatabaseHas('All_tab', ['number' => 5]);
     }
 }
