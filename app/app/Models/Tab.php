ce2cbb64460fc5e35b1e631a1edb5b1f9b7a3d29
Kajojan <kojoty1999@gmail.com>
2023-07-31 02:29:31 +0200

    tab Model

---
+++ app/app/Models/Tab.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tab extends Model
+{
+    protected $table = 'All_tab';
+
+    protected $fillable = [
+        'number',
+        'array', 
+    ];
+
+    public $timestamps = false;
+}
